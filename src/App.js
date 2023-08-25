2e672ee035848f91796bebf42cdde2c7c8ac8ea8
G.S <131451577+st-doval17@users.noreply.github.com>
2023-08-25 09:43:48 +0200

    Update
    - update App.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,73 @@
 // src/App.js
 
+import React, { useState, useEffect } from "react";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import "./App.css";
 
 const App = () => {
+  const [authUrl, setAuthUrl] = useState("");
+  const [authCode, setAuthCode] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [events, setEvents] = useState([]);
+
+  const handleGetAuthUrl = async () => {
+    try {
+      const response = await fetch(
+        "https://v5n9r94bfh.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const json = await response.json();
+      setAuthUrl(json.authUrl);
+    } catch (error) {
+      console.error("Error fetching auth URL:", error);
+    }
+  };
+
+  const handleGetAccessToken = async () => {
+    try {
+      const codeValue = encodeURIComponent(authCode);
+      const response = await fetch(
+        `https://v5n9r94bfh.execute-api.eu-central-1.amazonaws.com/dev/api/token/${codeValue}`
+      );
+      const json = await response.json();
+      setAccessToken(JSON.stringify(json));
+    } catch (error) {
+      console.error("Error fetching access token:", error);
+    }
+  };
+
+  const handleGetEvents = async () => {
+    try {
+      const response = await fetch(
+        `https://v5n9r94bfh.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/${JSON.parse(
+          accessToken
+        )}`
+      );
+      const json = await response.json();
+      setEvents(json);
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!accessToken) {
+      return;
+    }
+
+    handleGetEvents();
+  }, [accessToken]);
+
   return (
     <div className="App">
       <CitySearch />
       <EventList />
-      <NumberOfEvents className="number-of-events" />
+      <NumberOfEvents />
+      <button id="getEvents" onClick={handleGetEvents}>
+        Get Events
+      </button>
+      <pre id="events">{JSON.stringify(events, null, 2)}</pre>
     </div>
   );
 };
