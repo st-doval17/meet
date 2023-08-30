a3bc49b3f3ed37d7b3542c427c82f94504d350d6
G.S <131451577+st-doval17@users.noreply.github.com>
2023-08-30 09:26:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const App = () => {
 
   const fetchData = async () => {
     try {
+      setIsLoading(true);
       const allEvents = await getEvents();
       const filteredEvents =
         currentCity === "See all cities"
