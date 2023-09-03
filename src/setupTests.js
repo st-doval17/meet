25cad2135aefb06f6f81bc0e42943d6e97b22d68
G.S <131451577+st-doval17@users.noreply.github.com>
2023-09-03 14:56:58 +0200

    Update
    - update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -16,3 +16,20 @@ console.error = (...args) => {
   );
   if (!ignoreMessage) originalError(...args);
 };
+
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
